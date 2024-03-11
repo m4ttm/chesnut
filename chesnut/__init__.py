5c1314fc415d40a1ca27b56294c9050d3e35a59b
m4ttm <matt.mitchell97@hotmail.com>
2024-03-11 16:57:27 +0000

    fix: setting __doc__ in __init__.py to README.md in parent throws error if not found

--- chesnut/__init__.py
+++ chesnut/__init__.py
@@ -4,11 +4,12 @@ from .node import Node
 
 # pdoc home page - set the __doc__ attribute to the content of the README.md file
 _readme_path = os.path.join(os.path.dirname(__file__), '..', 'README.md')
-with open(_readme_path, 'r', encoding='utf-8') as readme_file:
-    readme_lines = readme_file.readlines()
+if os.path.exists(_readme_path):
+    with open(_readme_path, 'r', encoding='utf-8') as readme_file:
+        readme_lines = readme_file.readlines()
 # exclude the title, logo and any blank lines at the start of the README
-for i, line in enumerate(readme_lines):
-    if '<span class="pdoc-start">' in line:
-        readme_lines = readme_lines[i:]
-        break
-__doc__ = ''.join(readme_lines)
+    for i, line in enumerate(readme_lines):
+        if '<span class="pdoc-start">' in line:
+            readme_lines = readme_lines[i:]
+            break
+    __doc__ = ''.join(readme_lines)
